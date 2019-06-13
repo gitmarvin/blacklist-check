8f14a3ee92afd4f1d621adbde2ec295af396f59d
gitmarvin <46127242+gitmarvin@users.noreply.github.com>
2019-06-13 12:31:52 +0200

    Add files via upload

---
+++ blacklist-check.py
@@ -0,0 +1,31 @@
+import openpyxl
+import os
+
+os.chdir('blacklist-venv')
+
+blacklist = []
+checklist = []
+exclusionList = []
+
+def getblacklist():
+    blacklist_workbook = openpyxl.load_workbook(input('Blacklist File Name: ') + '.xlsx')
+    sheet = blacklist_workbook[input('Sheet Name:')]
+    for i in range(2, 300):
+        blacklist.append(sheet.cell(row=i, column=2).value)
+
+def getchecklist():
+    checklist_workbook = openpyxl.load_workbook(input('Checklist File Name: ') + '.xlsx')
+    cl_sheet = checklist_workbook[input('Sheet Name: ')]
+    for i in range(2, 900):
+        checklist.append(cl_sheet.cell(row=i, column=5).value)
+
+def blacklistcheck():
+    for i in blacklist:
+        if i in checklist and i is not None:
+            exclusionList.append(i)
+
+if __name__ == '__main__':
+    getblacklist()
+    getchecklist()
+    blacklistcheck()
+    print(exclusionList)
