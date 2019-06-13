e322e3f0e1ab76337d25104be8af0aa49a55c1e2
gitmarvin <46127242+gitmarvin@users.noreply.github.com>
2019-06-13 12:50:49 +0200

    Added input for max range

--- blacklist-check.py
+++ blacklist-check.py
@@ -10,13 +10,15 @@ exclusionList = []
 def getblacklist():
     blacklist_workbook = openpyxl.load_workbook(input('Blacklist File Name: ') + '.xlsx')
     sheet = blacklist_workbook[input('Sheet Name:')]
-    for i in range(2, 300):
+    maxrange = input('Max Range: ')
+    for i in range(2, maxrange):
         blacklist.append(sheet.cell(row=i, column=2).value)
 
 def getchecklist():
     checklist_workbook = openpyxl.load_workbook(input('Checklist File Name: ') + '.xlsx')
     cl_sheet = checklist_workbook[input('Sheet Name: ')]
-    for i in range(2, 900):
+    maxrange = input('Max Range: ')
+    for i in range(2, maxrange):
         checklist.append(cl_sheet.cell(row=i, column=5).value)
 
 def blacklistcheck():
